d64dda9c9375b0912e806620bfd6d73d056bb28d
kari-osk <karinaosuka@gmail.com>
2022-05-09 17:23:38 -0300

    MovieCard

--- frontend/src/components/Pages/Listing/index.tsx
+++ frontend/src/components/Pages/Listing/index.tsx
@@ -1,7 +1,19 @@
+import MovieCard from '../../MovieCard';
 import Pagination from '../../Pagination';
 
 function Listing() {
-	return <Pagination />;
+	return (
+		<>
+			<Pagination />;
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+						<MovieCard />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Listing;
