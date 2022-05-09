583756a0145089b317430af3fc00e6a9f6ed66ad
kari-osk <karinaosuka@gmail.com>
2022-05-09 16:47:56 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Listing from './components/Pages/Listing';
-import Form from './components/Pages/Listing';
+import Form from './components/Pages/Form';
 import Navbar from './components/Navbar';
 
 function App() {
